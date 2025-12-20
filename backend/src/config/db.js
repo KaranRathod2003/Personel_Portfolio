cbd76399a8633100cf5983f0e295a83206e6177f
Karan Rathod <kr04391@gmail.com>
2025-12-20 15:55:52 +0530

    feat : add DB connection

---
+++ backend/src/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+
+const connectDB = async()=>{
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URI}${DB_NAME}`)
+        console.log(`MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    }
+    catch(error){
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+}
+
+
+export { connectDB };
